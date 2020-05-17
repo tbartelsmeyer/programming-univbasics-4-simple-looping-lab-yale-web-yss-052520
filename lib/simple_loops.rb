a1947849a3db1153e105ae7e9e7ccf6a30e42d05
Tilman Bartelsmeyer <tilman.bartelsmeyer@yale.edu>
2020-05-17 05:49:58 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,27 @@
 # Write your methods here
+def loop_message_five_times(msg)
+  5.times do
+    puts msg
+  end
+end
+
+def loop_message_n_times(msg,amt2loop)
+   amt2loop.times do
+    puts msg
+  end
+end
+
+def output_array(array)
+  #i = 0
+  while array[i=0] do
+    puts array[i]
+    i += 1
+  end
+end
+
+output_array(["a","b","c"])
+
+
+def return_string_array
+  
+end
